1433887dc5f82821439fcd11434f21895371c4bc
antonkurin <antonkurin1@gmail.com>
2023-03-02 20:18:42 +0300

    Added modulation

---
+++ js/utils.js
@@ -0,0 +1,21 @@
+
+function getRandomPositiveInteger(a, b) {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+}
+
+function getRandomArrayElement(elements) {
+  return elements[getRandomPositiveInteger(0, elements.length - 1)];
+}
+
+function generateID () {
+  let counter = 0;
+  return function () {
+    ++counter;
+    return counter;
+  };
+}
+
+export {getRandomArrayElement, generateID, getRandomPositiveInteger};
