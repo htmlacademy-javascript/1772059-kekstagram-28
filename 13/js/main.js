f27e58cd1f9d5b696ac5d66fd92ccbcdaeecd432
Keks <keks@htmlacademy.ru>
2023-04-03 15:29:42 +0300

    :heavy_check_mark: Сборка #13

---
+++ 13/js/main.js
@@ -0,0 +1,5 @@
+import { getThumbnailsData } from './render-thumbnail-pictures.js';
+import { initPictureEditor } from './uploding-picture.js';
+
+initPictureEditor();
+getThumbnailsData();
---
+++ 13/js/render-thumbnail-pictures.js
@@ -0,0 +1,42 @@
+import { openFullSizePicture } from './open-full-size-picture.js';
+import { getData } from './api.js';
+import { showFatalErrorMessage } from './send-messages.js';
+
+const GET_URL = 'https://28.javascript.pages.academy/kekstagram/data';
+const ERROR_DELAY = 5000;
+
+const picturesGrid = document.querySelector('.pictures');
+const thumbnailTemplate = document.querySelector('#picture').content.querySelector('.picture');
+
+const createThumbnail = (picture) => {
+  const thumbnail = thumbnailTemplate.cloneNode(true);
+
+  thumbnail.querySelector('.picture__img').src = picture.url;
+  thumbnail.querySelector('.picture__img').alt = picture.description;
+  thumbnail.querySelector('.picture__likes').textContent = picture.likes;
+  thumbnail.querySelector('.picture__comments').textContent = picture.comments.length;
+
+  thumbnail.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    openFullSizePicture(picture);
+  });
+  return thumbnail;
+};
+
+const renderThumbnails = (data) => data.forEach((item) => picturesGrid.append(createThumbnail(item)));
+
+const onGetSuccess = (data) => renderThumbnails(data);
+
+const onGetFail = () => {
+  const errorBlock = showFatalErrorMessage();
+  document.body.append(errorBlock);
+
+  setTimeout(() => {
+    errorBlock.remove();
+  }, ERROR_DELAY);
+};
+
+const getThumbnailsData = () => getData(GET_URL, onGetSuccess, onGetFail);
+
+
+export { getThumbnailsData };
